d0429415701e0528141f8ea8a3864d387088a5cb
Eduardo Gil <pisangas@gmail.com>
2020-11-27 20:02:09 -0500

    Areas include

---
+++ AplicacionMVC/Areas/ModuloInventarios/ModuloInventariosAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AplicacionMVC.Areas.ModuloInventarios
+{
+    public class ModuloInventariosAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ModuloInventarios";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ModuloInventarios_default",
+                "ModuloInventarios/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
